891db778bc4b8c7e5278bc6d212cf3ff7d0a4cf0
yantaomeng <3094969938@qq.com>
2023-01-29 20:07:32 +0800

    feat:完善了请求的职责链

--- src/http/httpFactory.js
+++ src/http/httpFactory.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import Interceptor from './Interceptors';
+import Qs from 'qs';
+import Merge from '../merge/merge';
 export default class HttpFactory {
   constructor(options) {
+    this.merge=new Merge()
     this.instance = axios.create(options);
     this.interceptor = new Interceptor(this.instance);
     this.interceptor.addUtilsConfig(options);
@@ -12,9 +15,57 @@ export default class HttpFactory {
   setResponseInterceptors(success, error) {
     this.interceptor.response(success, error);
   }
+  setRequestHeadConfig(config){
+    const Type=config.type
+    const configTypeStrategyPond={
+        json:(config)=>{
+          config.headers = {
+            'Content-Type': 'application/json',
+          };
+        },
+        formData:(config)=>{
+          config.headers = {
+            'Content-Type': 'multipart/form-data',
+          };
+          const params = config.data;
+          let newParams = null;
+          if (params) {
+            newParams = new FormData();
+            for (const i in params) {
+              newParams.append(i, params[i]);
+            }
+          }
+          config.data = newParams;
+        },
+        urlencoded:(config)=>{
+          config.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          };
+          config.transformRequest = [
+            (data) => {
+              return Qs.stringify(data);
+            },
+          ];
+        }
+    }
+    configTypeStrategyPond[Type](config)
+    return config
+  }
+  setConfigFunctionLine(config){
+      const  processLine=[this.mergeRequestDefaultOptons,this.setRequestHeadConfig,(config)=>{
+        if (config.method === 'get') {
+          config.params = config.data;
+        }
+        return config
+      }]
+     return  processLine.reduce((before,after)=>{
+          return after(before)
+      },config)
+  }
   create() {
     return function(config){
-      this.instance()
+      config= this.setConfigFunctionLine(config)
+      return this.instance(config)
     }
   }
 }
