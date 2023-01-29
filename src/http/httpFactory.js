8877c5c8a93619e929d76e566dc68f6416d4de29
闫韬蒙 <3094969938@qq.com>
2023-01-30 01:26:04 +0800

    fix:修复了部分bug

--- src/http/httpFactory.js
+++ src/http/httpFactory.js
@@ -3,8 +3,8 @@ import Interceptor from './Interceptors';
 import Qs from 'qs';
 import Merge from '../merge/merge';
 export default class HttpFactory {
-  constructor(options={}) {
-    this.merge=new Merge()
+  constructor(options = {}) {
+    this.merge = new Merge();
     this.instance = axios.create(this.merge.getAxiosDefaultOptions(options.axiosOptions));
     this.interceptor = new Interceptor(this.instance);
     this.interceptor.addUtilsConfig(options);
@@ -15,57 +15,62 @@ export default class HttpFactory {
   setResponseInterceptors(success, error) {
     this.interceptor.response(success, error);
   }
-  setRequestHeadConfig(config){
-    const Type=config.type
-    const requestTypeStrategyPond={
-        json:(config)=>{
-          config.headers = {
-            'Content-Type': 'application/json',
-          };
-        },
-        formData:(config)=>{
-          config.headers = {
-            'Content-Type': 'multipart/form-data',
-          };
-          const params = config.data;
-          let newParams = null;
-          if (params) {
-            newParams = new FormData();
-            for (const i in params) {
-              newParams.append(i, params[i]);
-            }
+  setRequestHeadConfig(config) {
+    const Type = config.type;
+    const requestTypeStrategyPond = {
+      json: (config) => {
+        config.headers = {
+          'Content-Type': 'application/json',
+        };
+      },
+      formData: (config) => {
+        config.headers = {
+          'Content-Type': 'multipart/form-data',
+        };
+        const params = config.data;
+        let newParams = null;
+        if (params) {
+          newParams = new FormData();
+          for (const i in params) {
+            newParams.append(i, params[i]);
           }
-          config.data = newParams;
-        },
-        urlencoded:(config)=>{
-          config.headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          };
-          config.transformRequest = [
-            (data) => {
-              return Qs.stringify(data);
-            },
-          ];
         }
-    }
-    requestTypeStrategyPond[Type](config)
-    return config
+        config.data = newParams;
+      },
+      urlencoded: (config) => {
+        config.headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        };
+        config.transformRequest = [
+          (data) => {
+            return Qs.stringify(data);
+          },
+        ];
+      },
+    };
+    requestTypeStrategyPond[Type](config);
+    return config;
   }
-  setConfigFunctionLine(config){
-      const  processLine=[this.mergeRequestDefaultOptons,this.setRequestHeadConfig,(config)=>{
+  setConfigFunctionLine(config) {
+    const processLine = [
+      this.merge.mergeRequestDefaultOptons.bind(this.merge),
+      this.setRequestHeadConfig,
+      (config) => {
         if (config.method === 'get') {
           config.params = config.data;
         }
-        return config
-      }]
-      return  processLine.reduce((before,after)=>{
-          return after(before)
-      },config)
+        return config;
+      },
+    ];
+    return processLine.reduce((before, after, index) => {
+      return processLine[index](before);
+    }, config);
   }
   create() {
-    return function(config){
-      config= this.setConfigFunctionLine(config)
-      return this.instance(config)
-    }
+    const that = this;
+    return function (config) {
+      config = that.setConfigFunctionLine(config);
+      return that.instance.call(this, config);
+    };
   }
 }
--- src/merge/merge.js
+++ src/merge/merge.js
@@ -1,24 +1,24 @@
-export default class Merge{
-    constructor(){}
-    mergeObjectOptions(parentConfig,childConfig){
-      return  Object.assign(parentConfig, childConfig);
-    }
-    getRequestDefaultOptions(){
-        return {
-            method: 'post',
-            type: 'json',
-        }
-    }
-    getAxiosDefaultOptions(){
-      return {
-        timeout:5000,
-        baseUrl:''
-      }
-    }
-    mergeAxiosDefaultOptons(config={}){
-      return  this.mergeObjectOptions(this.getAxiosDefaultOptions(),config)
-    }
-    mergeRequestDefaultOptons(config={}){
-      return  this.mergeObjectOptions(this.getRequestDefaultOptions(),config)
-    }
+export default class Merge {
+  constructor() {}
+  mergeObjectOptions(parentConfig, childConfig) {
+    return Object.assign(parentConfig, childConfig);
+  }
+  getRequestDefaultOptions() {
+    return {
+      method: 'post',
+      type: 'json',
+    };
+  }
+  getAxiosDefaultOptions() {
+    return {
+      timeout: 5000,
+      baseUrl: '',
+    };
+  }
+  mergeAxiosDefaultOptons(config = {}) {
+    return this.mergeObjectOptions(this.getAxiosDefaultOptions(), config);
+  }
+  mergeRequestDefaultOptons(config = {}) {
+    return this.mergeObjectOptions(this.getRequestDefaultOptions(), config);
+  }
 }
